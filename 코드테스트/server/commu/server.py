04080ee933f35872e6fc7188cf35363150f8c8d4
cflab2017 <cflab@naver.com>
2025-08-14 13:03:32 +0900

    aa

--- 코드테스트/server/commu/server.py
+++ 코드테스트/server/commu/server.py
@@ -138,9 +138,12 @@ class socketServer():
                 with open(filename, 'rb') as fr:
                     login_dict = pickle.load(fr)
                     self.login_dict =  login_dict
-                    for name in self.login_dict:
-                        if self.login_dict[name] is None:
-                            del self.login_dict[name]
+                names = []
+                for name in self.login_dict:
+                    if self.login_dict[name] is None:
+                        names.append(name)
+                for name in names:
+                    del self.login_dict[name]
         if state == 'w':  
             with open(filename, 'wb') as fw:
                 pickle.dump(self.login_dict,fw)
